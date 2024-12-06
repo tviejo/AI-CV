b6ebb055803348fb20389aa9ccc3acf04d01f2d9
tviejo <tviejo@student.42.fr>
2024-12-06 22:14:51 +0100

    added analytics

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Chat from './components/Chat';
 import cvData from './data/cvData';
 import { ThemeProvider } from './context/ThemeContext';
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
